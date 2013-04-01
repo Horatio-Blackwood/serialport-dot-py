b00d8881da9fbd7dd00775132c133b4bd355152c
Adam Anderson <horatio.blackwood@gmail.com>
2013-03-31 22:46:58 -0700

    Update serialconfig.py

--- serialconfig.py
+++ serialconfig.py
@@ -1,7 +1,7 @@
 # Author:  Adam Anderson
 # Depends on:  Python 3.3
 # Created:  February 12, 2012
-# Updated:  March 31th, 2013
+# Updated:  March 31st, 2013
 
 import configparser
 
@@ -14,19 +14,29 @@ class SerialConfig(object):
     """ A class that contains all the config properties for Serial communication
         with the Arduino.
     """
+    
+    def __init__(self, configName='serial'):
+        """ Constructs anew instance of SerialConfig.
+            
+            Param:
+                configName:  The name of the section of the config file to read 
+                in the serial config data from.  Default value is 'serial' if no
+                other value is specified.
+        """
+        self.configName = configName
 
     # The serial port's identifier (this is '/dev/tty/AM0' on Unix-like systems or 'COM4' on windows systems)
-    port      = config.get('serial', 'port')
+    port      = config.get(self.configName, 'port')
 
     # The baudrate to use for serial communications.
-    baud      = config.getint('serial', 'baud')
+    baud      = config.getint(self.configName, 'baud')
 
     # The 'go-ahead' byte configured, if any.  When the serial port
     # receives this byte it knows its okay to transmit more bytes to
     # the target device.
-    readybyte = config.getint('serial', 'readybyte')
+    readybyte = config.getint(self.configName, 'readybyte')
 
     # the configured 'wait byte', if any.  When the SerialPort gets
     # this byte it waits until receiving the readbyte before continuing
     # to xmit more data.
-    waitbyte  = config.getint('serial', 'waitbyte')
+    waitbyte  = config.getint(self.configName, 'waitbyte')
