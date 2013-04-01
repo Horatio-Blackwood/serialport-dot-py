5a7548d2596c0f3a7d21b14f2f865c4c639a7104
Adam Anderson <horatio.blackwood@gmail.com>
2013-03-31 22:51:16 -0700

    Update serialconfig.py

--- serialconfig.py
+++ serialconfig.py
@@ -5,17 +5,12 @@
 
 import configparser
 
-# Create a config parser and read in the properties
-config = configparser.RawConfigParser()
-config.read('serial.cfg')
-
-
 class SerialConfig(object):
     """ A class that contains all the config properties for Serial communication
         with the Arduino.
     """
     
-    def __init__(self, configName='serial'):
+    def __init__(self, configName='serial', configFileName='config.cfg'):
         """ Constructs anew instance of SerialConfig.
             
             Param:
@@ -23,20 +18,23 @@ class SerialConfig(object):
                 in the serial config data from.  Default value is 'serial' if no
                 other value is specified.
         """
+        # Create a config parser and read in the properties
+        config = configparser.RawConfigParser()
+        config.read(configFileName)
         self.configName = configName
 
-    # The serial port's identifier (this is '/dev/tty/AM0' on Unix-like systems or 'COM4' on windows systems)
-    port      = config.get(self.configName, 'port')
+        # The serial port's identifier (this is '/dev/tty/AM0' on Unix-like systems or 'COM4' on windows systems)
+        self.port = config.get(self.configName, 'port')
 
-    # The baudrate to use for serial communications.
-    baud      = config.getint(self.configName, 'baud')
+        # The baudrate to use for serial communications.
+        self.baud = config.getint(self.configName, 'baud')
 
-    # The 'go-ahead' byte configured, if any.  When the serial port
-    # receives this byte it knows its okay to transmit more bytes to
-    # the target device.
-    readybyte = config.getint(self.configName, 'readybyte')
-
-    # the configured 'wait byte', if any.  When the SerialPort gets
-    # this byte it waits until receiving the readbyte before continuing
-    # to xmit more data.
-    waitbyte  = config.getint(self.configName, 'waitbyte')
+        # The 'go-ahead' byte configured, if any.  When the serial port
+        # receives this byte it knows its okay to transmit more bytes to
+        # the target device.
+        self.readybyte = config.getint(self.configName, 'readybyte')
+    
+        # the configured 'wait byte', if any.  When the SerialPort gets
+        # this byte it waits until receiving the readbyte before continuing
+        # to xmit more data.
+        self.waitbyte = config.getint(self.configName, 'waitbyte')
